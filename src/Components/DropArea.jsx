942a450355d1a3fee2e32872b8d24df312618961
sachindr66 <sachindrhsn@gmail.com>
2024-08-03 02:35:18 +0530

    updated

--- src/Components/DropArea.jsx
+++ src/Components/DropArea.jsx
@@ -1,22 +1,32 @@
-import React, { useState, useSyncExternalStore } from 'react'
-import './DropArea.css'
+import React, { useState } from 'react';
+import './DropArea.css';
 
-const DropArea = ({onDrop}) => {
+const DropArea = ({ onDrop }) => {
+  const [showDrop, setShowDrop] = useState(false);
 
-    const [showDrop, setShowDrop]=useState(false)
+  const handleDrop = (e) => {
+    e.preventDefault();
+    onDrop();
+    setShowDrop(false);
+  };
+
   return (
-    <div 
-    onDragEnter={()=>{setShowDrop(true)}}
-    onDragLeave={()=>{setShowDrop(false)}}
-    onDrop={()=>{
-        onDrop();
-        setShowDrop(false)
-    }}
-    onDragOver={e => e.preventDefault()}
-    className={showDrop? "drop_area": "hide_drop"}>
-      here
+    <div
+      onDragEnter={() => setShowDrop(true)}
+      onDragLeave={() => setShowDrop(false)}
+      onDrop={handleDrop}
+      onDragOver={(e) => e.preventDefault()}
+      onTouchStart={() => setShowDrop(true)}
+      onTouchEnd={(e) => {
+        e.preventDefault();
+        setShowDrop(false);
+      }}
+      onTouchMove={(e) => e.preventDefault()}
+      className={showDrop ? 'drop_area' : 'hide_drop'}
+    >
+      Here
     </div>
-  )
-}
+  );
+};
 
-export default DropArea
+export default DropArea;
